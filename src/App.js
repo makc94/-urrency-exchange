7ae3e6c6c0ed9abd0a28bb75074ab8d25505e067
Maksym <makclan994@gmail.com>
2021-05-11 21:00:57 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./index.css";
 
 export default function App() {
   const [ukr, setUkr] = useState("");
   const [dol, setDol] = useState("");
+  const [kyrsDol, setKyrsDol] = useState("");
+
+  useEffect(() => {
+    const URLrequest =
+      "http://api.currencylayer.com/live?access_key=ff13dab9da2cea673aecc73c898e4daa&currencies=USD,UAH";
+    fetch(URLrequest)
+      .then((response) => response.json())
+      .then((res) => {
+        setKyrsDol(res.quotes.USDUAH);
+      });
+  }, []);
 
   const toDollar = (e) => {
     const cur = e.target.value;
     setUkr(cur);
-    setDol(cur / 27.8);
+    setDol(cur / kyrsDol);
   };
 
   const toGrivna = (e) => {
     const cur = e.target.value;
     setDol(cur);
-    setUkr(cur * 26.8);
+    setUkr(cur * kyrsDol);
+  };
+
+  const toggle = () => {
+    document.body.classList.toggle("dark");
+
+    // const flip = document.getElementById("button");
+    // flip.classList.add("flip");
   };
 
   return (
-    <div>
-      <form>
-        <label>
-          <span>Grivna: </span>
-          <input type="text" value={ukr} onChange={toDollar} />
-        </label>
-        <label>
-          <span>Dollar: </span>
-          <input type="text" value={dol} onChange={toGrivna} />
-        </label>
-      </form>
+    <div className="container">
+      <div className="d-flex align-items-center common">
+        <div className="col">
+          <label>
+            <span className="display-6">Grivna: </span>
+            <input
+              className="form-control fadeInLeftBig"
+              type="text"
+              value={ukr}
+              placeholder="uah"
+              onChange={toDollar}
+            />
+          </label>
+        </div>
+        <div className="col">
+          <label>
+            <span className="display-6">Dollar: </span>
+            <input
+              className="form-control fadeInDownBig"
+              type="text"
+              value={dol}
+              placeholder="usd"
+              onChange={toGrivna}
+            />
+          </label>
+        </div>
+      </div>
+      <div className="col click">
+        <button
+          id="button"
+          type="button"
+          className="btn btn-primary flip"
+          onClick={toggle}
+        >
+          Change theme
+        </button>
+      </div>
     </div>
   );
 }
